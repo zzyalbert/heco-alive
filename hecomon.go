b9b85196beac727566123cd30eaad4eaa155db27
wangsai <wangsai@huobi.com>
2021-02-26 13:36:27 +0800

    1. eth.sync api; 2. getBlock -> getBlockHeader; 3. lock for state; 4.  stop with child process；

--- hecomon.go
+++ hecomon.go
@@ -3,26 +3,29 @@ package main
 import (
 	"context"
 	"fmt"
-	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethereum/go-ethereum/ethclient"
-	"gopkg.in/natefinch/lumberjack.v2"
 	"log"
 	"math/big"
 	"os"
 	"os/exec"
+	"os/signal"
 	"strings"
+	"syscall"
 	"time"
+
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"gopkg.in/natefinch/lumberjack.v2"
 )
 
 const (
 	rpcHost       = "http://127.0.0.1:8545"
 	fetchInterval = 3
-	killCount     = 100
+	killCount     = 10
 )
 
 type State struct {
 	height uint64
-	block  *types.Block
+	block  *types.Header
 	count  int64
 }
 
@@ -31,9 +34,10 @@ func (m State) String() string {
 }
 
 var (
-	lastState *State
-	childCmd  *exec.Cmd
-	args      []string
+	lastState  *State
+	clearState chan int
+	childCmd   *exec.Cmd
+	args       []string
 )
 
 func main() {
@@ -46,45 +50,77 @@ func main() {
 	args = os.Args
 
 	log.SetOutput(&lumberjack.Logger{
-		Filename:   "./hecomon.log",
+		Filename:   "./logs/hecomon.log",
 		MaxSize:    10, // megabytes
 		MaxBackups: 4,
 		MaxAge:     28,    //days
 		Compress:   false, // disabled by default
 	})
 
+	clearState = make(chan int, 10)
+
 	go monitorLoop()
 
 	for {
 		runApp()
+		time.Sleep(1 * time.Second)
 	}
 }
 
 func runApp() {
 	log.Printf("start command with args %s", strings.Join(args[1:], " "))
 
+	//clear state
+	clearState <- 0
+
 	childCmd = exec.Command(args[1], args[2:]...)
 	childCmd.Stdout = os.Stdout
 	childCmd.Stderr = os.Stderr
 	childCmd.Stdin = os.Stdin
-
 	if err := childCmd.Run(); err != nil {
 		log.Printf("error in start %s \n", err)
 	}
-
-	//clear state
-	clearState()
 }
 
 func monitorLoop() {
 	t := time.NewTicker(fetchInterval * time.Second)
 
+	sigc := make(chan os.Signal, 1)
+	signal.Notify(sigc, syscall.SIGTERM, syscall.SIGINT)
+
 	for {
 		select {
+		case <-sigc:
+			kill()
+			os.Exit(0)
 		case <-t.C:
 			fetchBlock()
+		case <-clearState:
+			lastState = nil
+			log.Println("clear state")
 		}
 	}
+}
+
+func getCurrentHeight(client *ethclient.Client) (height uint64, err error) {
+
+	process, err := client.SyncProgress(context.Background())
+	if err != nil {
+		return
+	}
+
+	//syncing
+	if process != nil {
+		height = process.CurrentBlock
+		return
+	}
+
+	height, err = client.BlockNumber(context.Background())
+	if err != nil {
+		return
+	}
+
+	return
 }
 
 func fetchBlock() {
@@ -97,7 +133,7 @@ func fetchBlock() {
 
 	log.Println("fetch block at", time.Now())
 
-	height, err := rpcClient.BlockNumber(context.Background())
+	height, err := getCurrentHeight(rpcClient)
 	if err != nil || height == 0 {
 		log.Printf("erro in block height, %s \n", err)
 		handleErr()
@@ -106,35 +142,36 @@ func fetchBlock() {
 
 	log.Println("read height:", height)
 
-	block, err := rpcClient.BlockByNumber(context.Background(), new(big.Int).SetUint64(height))
-	if err != nil || block == nil {
+	header, err := rpcClient.HeaderByNumber(context.Background(), new(big.Int).SetUint64(height))
+	if err != nil || header == nil {
 		log.Printf("erro in block %s, \n ", err)
 		handleErr()
 		return
 	}
 
-	log.Printf("read block %d -> %s: \n", block.Header().Number, block.Hash().String())
+	log.Printf("read block %d -> %s: \n", header.Number, header.Hash().String())
 
 	//count
-	count(height, block)
+	count(height, header)
 }
 
-func count(height uint64, block *types.Block) {
+func count(height uint64, header *types.Header) {
 	if lastState == nil {
-		lastState = &State{height, block, 0}
-	} else {
-		//count stuck
-		if height <= lastState.height {
-			lastState.count++
-			printStatus()
-		} else {
-			lastState.height = height
-			lastState.count = 0
-			printStatus()
-		}
+		lastState = &State{height, header, 0}
+		return
 	}
 
-	tryKill()
+	//count stuck
+	if height <= lastState.height {
+		lastState.count++
+		tryKill()
+		return
+	}
+
+	lastState.height = height
+	lastState.block = header
+	lastState.count = 0
+	printStatus()
 }
 
 func tryKill() {
@@ -143,16 +180,8 @@ func tryKill() {
 	printStatus()
 
 	if lastState != nil && lastState.count >= killCount {
-		ret := kill()
-		if ret {
-			clearState()
-		}
+		kill()
 	}
-}
-
-func clearState() {
-	lastState = nil
-	log.Println("clear state")
 }
 
 func kill() bool {
